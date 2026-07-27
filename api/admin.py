1a8d14031fb09ec769ba9c4db09211c5a2a6dc7e
mehmetKrca <mehmet16.karaca16@gmail.com>
2026-07-27 12:25:06 +0300

    Delete api directory

--- api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-# Mevcut modellerinin (Proje, DogramaKalemi vb.) yanına FiyatAyarlari'ni da ekle
-from .models import Proje, DogramaKalemi, FiyatAyarlari 
-
-# Eski kayıtların duruyorsa onlara dokunma, sadece en alta şunu ekle:
-admin.site.register(FiyatAyarlari)
